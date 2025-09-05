40afd2fe70de9cd2729d5cf141faff4ef04e7a49
Debjyoti Shit <debyotishit27@gmail.com>
2025-09-05 22:46:43 +0530

    Add initial application manifests

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to the Express server!'});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
